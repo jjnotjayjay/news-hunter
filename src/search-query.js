1e34c2b6bc235d7a54c1dd9448a83503044c0653
jjnotjayjay <jj@alumni.usc.edu>
2018-11-20 14:06:14 -0800

    Stored value of text input box in state and updates state on value change; index.js cleaned. #1

--- src/search-query.js
+++ src/search-query.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class SearchQuery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchTopic: ''
+    }
+    
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ searchTopic: e.target.value })
+  }
+
   render() {
+    const { searchTopic } = this.state
+    const { handleChange } = this
+
     return (
       <div>
-        <input type="text" placeholder="Topic to search" />
+        <input type="text" placeholder="Topic to search" value={searchTopic} onChange={handleChange}/>
         <button type="submit">Search!</button>
       </div>
     )
