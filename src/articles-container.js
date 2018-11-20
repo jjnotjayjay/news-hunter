6bf47312f7cefeaed7b8ab7e49ebc43b3e05f5c0
jjnotjayjay <jj@alumni.usc.edu>
2018-11-20 15:19:44 -0800

    Articles with multimedia render a thumbnail; articles without render a placeholder. #1

--- src/articles-container.js
+++ src/articles-container.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ArticleThumbnail from './article-thumbnail.js'
 
-export default class ArticlesContainer extends Component {
-  render() {
-    const { articles } = this.props
+export default function ArticlesContainer(props) {
+  const { articles } = props
 
-    return articles.map(article => {
-      return (
-        <div>
-          <h4>{article.headline.main}</h4>
-          <h6>{article.pub_date.slice(0, 10)}</h6>
-          <p>{article.snippet}</p>
-        </div>
-      )
-    })
-  }
+  return articles.map(article => {
+    return (
+      <div>
+        <h4>{article.headline.main}</h4>
+        <h6>{article.pub_date.slice(0, 10)}</h6>
+        <ArticleThumbnail image={article.multimedia} />
+        <p>{article.snippet}</p>
+      </div>
+    )
+  })
 }
