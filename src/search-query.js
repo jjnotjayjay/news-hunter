3b57a052c258c5466688fa68875bfa7afd3cb7c9
jjnotjayjay <jj@alumni.usc.edu>
2018-11-20 17:27:01 -0800

    Improved styling of the text input and submit buttons. #1

--- src/search-query.js
+++ src/search-query.js
@@ -1,4 +1,38 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: 5vh 0;
+`
+
+const QueryInput = styled.input`
+  margin-right: 10px;
+  padding: 2px 5px;
+  background: rgba(255, 255, 255, 0.6);
+  border: 1px solid #444;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-family: 'EB Garamond', serif;
+  transition: background 0.5s;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.85)
+  }
+`
+
+const SearchButton = styled.button`
+  background: rgba(255, 255, 255, 0.6);
+  border: 1px solid #444;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-family: 'EB Garamond', serif;
+  transition: background 0.5s, color 0.5s;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
+`
 
 export default class SearchQuery extends Component {
   constructor(props) {
@@ -20,10 +54,10 @@ export default class SearchQuery extends Component {
     const { handleChange } = this
 
     return (
-      <div>
-        <input type="text" placeholder="Topic to search..." value={searchTopic} onChange={handleChange}/>
-        <button onClick={() => requestArticles(searchTopic)}>Search!</button>
-      </div>
+      <Wrapper>
+        <QueryInput type="text" placeholder="Topic to search..." value={searchTopic} onChange={handleChange}/>
+        <SearchButton onClick={() => requestArticles(searchTopic)}>Hunt!</SearchButton>
+      </Wrapper>
     )
   }
 }
