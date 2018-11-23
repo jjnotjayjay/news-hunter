29829fbe94eb0b10872f55e3b0771162060bf4d1
jjnotjayjay <jj@alumni.usc.edu>
2018-11-23 11:43:19 -0800

    Updates

---
+++ precache-manifest.bda3f23e6628d64ea43bf65df665da0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "77e0386d0f87cf1d35d6",
+    "url": "/news-hunter/static/js/runtime~main.77e0386d.js"
+  },
+  {
+    "revision": "7d8ed9d24ab2fcb3a7fb",
+    "url": "/news-hunter/static/js/main.7d8ed9d2.chunk.js"
+  },
+  {
+    "revision": "53307dc2c09456a8a1c1",
+    "url": "/news-hunter/static/js/1.53307dc2.chunk.js"
+  },
+  {
+    "revision": "d44fe8eef0dc19fedb5c8c37f8cbf6d0",
+    "url": "/news-hunter/index.html"
+  }
+];
